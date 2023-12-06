111fac6748459cfc2e865e80a9e12e45392017a1
Vineet Kumar Gupta <136819718+vinparabyte@users.noreply.github.com>
2023-12-06 19:02:56 +0000

    datatypes

---
+++ 01_Basic/02_dataType.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3); // we are using nodejs, not browser
+console.log(3 + 3);
+console.log("hello");
+
+let name = "Vineet";
+let age = 23;
+let isLoggedIn = false;
+
+//Datatypes in JS
+/*
+Primitive Data Type
+number - 2 to power 53,
+bigint,
+string => "";,
+boolean => true/false,
+null = standalone value,
+undefined = not defined,
+symbol = unique
+*/
+
+//object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null);  //object
