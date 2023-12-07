e79b1aae7f04e98d887920f22da9b04429f56427
Vineet Kumar Gupta <136819718+vinparabyte@users.noreply.github.com>
2023-12-07 04:37:28 +0000

    data update

--- 01_Basic/02_dataType.js
+++ 01_Basic/02_dataType.js
@@ -1,12 +1,12 @@
-"use strict"; //treat all JS code as newer version
+// "use strict"; //treat all JS code as newer version
 
-// alert(3+3); // we are using nodejs, not browser
-console.log(3 + 3);
-console.log("hello");
+// // alert(3+3); // we are using nodejs, not browser
+// console.log(3 + 3);
+// console.log("hello");
 
-let name = "Vineet";
-let age = 23;
-let isLoggedIn = false;
+// let name = "Vineet";
+// let age = 23;
+// let isLoggedIn = false;
 
 //Datatypes in JS
 /*
@@ -22,5 +22,23 @@ symbol = unique
 
 //object
 
-console.log(typeof undefined); //undefined
-console.log(typeof null);  //object
+// console.log(typeof undefined); //undefined
+// console.log(typeof null);  //object
+
+// Primitive
+// 7 types : String,Number,Boolean,Null,Undefined,Symbol,BigInt
+
+const score = 100;
+const scoreValue = 100.3;
+
+const isLoggedIn = false;
+const outsideTemp = null;
+let userEmail;
+
+const id = Symbol('123');
+const anotherId = Symbol('123');
+
+console.log(id === anotherId);
+
+// Reference (Non Primitive)
+// Array, Objects, Function
