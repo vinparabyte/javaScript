2ec98b248af721869d8a836c9791e07f0546487d
Vineet Kumar Gupta <136819718+vinparabyte@users.noreply.github.com>
2023-12-07 03:56:36 +0000

    Operation

--- 01_Basic/03_conversionOperation.js
+++ 01_Basic/03_conversionOperation.js
@@ -22,5 +22,35 @@ let isLoggedIn = 1;
 let someNumber = 33;
 
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//*******************OPERATAIONS**************************************************
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%3);
+
+let str1 = "Hello";
+let str2 = " Vineet";
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+"2");
+// console.log(1+2+"2");
+
+// console.log(true);
+
+let gameCounter = 100;
+++gameCounter;
+console.log(gameCounter);
