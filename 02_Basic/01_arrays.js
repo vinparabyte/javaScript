4aae89da5e58a44a0fee67f33a01eb56b803dea5
Vineet Kumar Gupta <136819718+vinparabyte@users.noreply.github.com>
2023-12-08 05:30:41 +0000

    array

---
+++ 02_Basic/01_arrays.js
@@ -0,0 +1,27 @@
+// Array
+
+const myArr = [0,1,2,3,4,5];
+const myHeroes = ["spiderman","shaktiman","ironman"];
+
+const myArr2 = new Array(1,2,3,4);
+// console.log(myArr[3]);
+
+// Array methods
+
+// myArr.push(6);
+// myArr.push(7);
+// myArr.pop();
+
+// myArr.unshift(9);
+// myArr.shift();
+
+// console.log(myArr.includes(9))
+// console.log(myArr.includes(5));
+// console.log(myArr.indexOf(4));
+
+// const newArr = myArr.join();
+
+// console.log(myArr);
+// console.log(newArr);
+
+
